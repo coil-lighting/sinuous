30fbe5d2eecd3981320602d4f9010cfbcd2782f8
Mike Bissell <src@coil-lighting.com>
2014-05-17 14:43:27 -0700

    TST: how to put functions inside of structs, including generic functions

---
+++ example/fn_pointer.rs
@@ -0,0 +1,42 @@
+// How to use function pointers inside of structs, how to get them out and
+// call the functions, and how to do it with generic <T> typed functions, too.
+
+fn double(x: i64) -> i64 {
+    x * 2
+}
+
+fn noop<T>(x: T) -> T {
+    x
+}
+
+fn _noop(x: i64) -> i64 {
+    x
+}
+
+#[test]
+fn test_double() {
+    assert!(double(0) == 0);
+    assert!(double(3) == 6);
+}
+
+struct Ab {
+    function_a: fn(i64) -> i64,
+    function_b: fn(i64) -> i64,
+}
+
+#[test]
+fn test_astruct() {
+    let ab = Ab {
+        function_a: double,
+
+        // don't put "noop<i64>"" here because function_b was defined as fn(i64) -> i64
+        function_b: noop,
+    };
+    let fa = ab.function_a;
+    assert!(fa(0) == 0);
+    assert!(fa(3) == 6);
+
+    let fb = ab.function_b;
+    assert!(fb(0) == 0);
+    assert!(fb(3) == 3);
+}
