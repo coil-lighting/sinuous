2e36db5e9b0146fdeb6b56021c645934b1544940
Mike Bissell <src@coil-lighting.com>
2014-06-04 11:13:43 -0700

    DOC: expand on docstrings

--- src/sinuous.rs
+++ src/sinuous.rs
@@ -2,21 +2,29 @@
 #![allow(dead_code)]
 #![allow(non_camel_case_types)]
 
-//! Nude pencil drawings of basic libsinuous constructs.
+//! Libsinuous provides and efficient, portable and flexible foundation library
+//! for describing and controlling the parameters of DMX lights (especially
+//! moving lights), mechanical effects, 2D motion graphics scenes, 3D models,
+//! MIDI instruments, and audio plugin instruments such as VSTs and VSTis.
+//!
+//! Libsinuous seeks to abstract high-level aesthetic control from low-level
+//! protocol mappings, and unlike most controllers, it happily allows the user
+//! to build assemblies of off the shelf instruments and components into large
+//! assemblies with coherently unified behaviors.
+//!
+//! This is the fourth generation of the LD50 project, which has been used in
+//! various forms in events and theatrical productions for most of the past
+//! decade.
 //
 // This is just a napkin sketch for the port of rbld50 core from Ruby to Rust.
 // There is a LOT more to come. Fortunately the Ruby stuff mostly tests and runs
 // okay.
 //
 // This is not a terribly well-organized, well-documented or well thought-out
-// module just yet. It is only my very first Rust program since FuzzBuzz and
+// module just yet. It is our very first Rust program aside from FizzBuzz and
 // Hello, World. Expect major reorganization.
 //
-// It compiled under 0.11-pre.
-//
-// # Comments that look like this are just copied verbatim from the Ruby version.
-// # They do not necessarily pertain to this Rust version. Just for reference
-// # while I try to port this puppy.
+// It compiles under the most recent 0.11-pre.
 
 mod blend;
 mod device;
