74303c020c60c11e259b5a3a7a1342e532b0841f
Mike Bissell <src@coil-lighting.com>
2014-06-10 12:16:41 -0700

    REF: move dimmer example into its own TST

--- src/sinuous.rs
+++ src/sinuous.rs
@@ -29,14 +29,7 @@
 extern crate debug;
 extern crate time;
 
-use time::*;
-
-use device::*;
-use dmx::*;
-use effect::*;
-use render::*;
-use topo::*;
-
+use test_dimmer::*; // TODO: figure out how to move test modules to a subdirectory
 
 mod blend;
 mod device;
@@ -45,84 +38,11 @@ mod effect;
 mod numeric;
 mod range;
 mod render;
+mod test_dimmer;
 mod topo;
 mod world;
 
 
 fn main() {
-    // TODOMB move to its own file
-
-    let p = Profile{
-        name: "Dimmer".to_string(),
-        nickname: "Dim".to_string(),
-        manufacturer: "Generic".to_string(),
-        author: "Chris Macklin".to_string(),
-        date: "June 7, 2014".to_string(),
-        version: 0,
-        chan_alloc: DmxSingleton(1),
-        root: Attr(Attribute{
-            name: "Dimmer".to_string(),
-            effect: (Dimmer, ColorspaceI, Value),
-            topo: &'static continuous_euclidian_unipolar,
-            default: Some(Continuous(0.)),
-            dmx: Some(DmxMap{
-                offset: DmxAddressOffsetSingle(0),
-                renderer: DmxFloatRenderer(render_dmx_float_unipolar)
-            })
-        })
-    };
-
-    // every device branch must refer to a profile branch.  this doesn't make sense
-    // for the root of the device tree.  might want to consider special-casing this,
-    // or it may just not matter.
-    let dummy_pbranch = ProfileBranch{
-        name: "Device tree root".to_string(),
-        nickname: "DevTrRt".to_string(),
-        children: Vec::new()
-    };
-
-    let mut dev_tree_root = DeviceBranch{
-        profile_branch: &dummy_pbranch,
-        children: Vec::new() // TODOMB defensive commas
-    };
-
-    // TODOMB import before using, make a factory method
-    let univ = std::rc::Rc::new(std::cell::RefCell::new(DmxUniverse{id: 0, name: "U1".to_string(), frame: [0, ..512]}));
-
-    let mut devs = Vec::new();
-
-    for i in range(0u, 256u) {
-        devs.push(patch(&p, &mut dev_tree_root, i, univ.clone()).unwrap());
-    }
-
-    fn write_dimmer_val(dim: &Device, v: f64) {
-        match *dim.root {
-            DeviceNodeEndpoint(ref e) => e.set_val(Continuous(v)),
-            _ => ()
-        }
-    }
-
-    let (mut t0, mut t1): (f64, f64);
-
-    let mut tot = 0f64;
-
-    let n_trial = 10000;
-
-    for n in range(0,n_trial) {
-        let v = (n as f64)/9999f64;
-
-        t0 = precise_time_s();
-        for d in devs.mut_iter() {
-            write_dimmer_val(d,v);
-            d.render();
-        }
-        t1 = precise_time_s();
-
-        tot += t1 - t0;
-
-        //println!("{:?}", univ.borrow().frame);
-    }
-
-    println!("Average time: {} ms", (1000f64 * tot) / (n_trial as f64));
-
+    test_dimmer();
 }
